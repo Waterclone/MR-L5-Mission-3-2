e6fe657d48810e4a3de0a9269548d2abeed648db
Jimson Montederamos <Jimson@missionreadyhq.com>
2023-08-30 07:29:39 +1200

    quote test cases passed

--- src/services/quoteServices.ts
+++ src/services/quoteServices.ts
@@ -1,6 +1,17 @@
 export const calculateQuote = (car_value: number, risk_rating: number) => {
-  if (!car_value || !risk_rating || risk_rating > 5 || risk_rating < 1) {
-    return { error: "Missing parameters" };
+  if (!car_value || !risk_rating || typeof risk_rating !== 'number' ) {
+    return { error: "Missing or Invalid parameters" };
+  }
+
+
+  if (car_value.toString().length > 9) {
+    return { error: "Car value is too high" };
+  }
+  if (car_value < 0) {
+    return { error: "Invalid car value" };
+  }
+  if (risk_rating > 5 || risk_rating < 1) {
+    return { error: "Invalid risk rating" };
   }
 
   const calculatePremium = ({ car_value, risk_rating }: { car_value: number; risk_rating: number }) => {
@@ -16,3 +27,4 @@ export const calculateQuote = (car_value: number, risk_rating: number) => {
 
   return { premiumData };
 };
+
