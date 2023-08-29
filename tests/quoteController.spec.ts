116c593e412528dc0b2832965635c7fac7a333ec
Jimson Montederamos <Jimson@missionreadyhq.com>
2023-08-30 00:59:27 +1200

    adding test cases in quote API

--- tests/quoteController.spec.ts
+++ tests/quoteController.spec.ts
@@ -1,11 +1,11 @@
 import { describe, it, expect } from "@jest/globals";
 import { calculateQuote } from "../src/services/quoteServices";
 
-describe("premium quote tests", () => {
-  it("should return monthly premium: 22 and yearly premium: 264. sunshine scenario", () => {
+describe("calculateQuote function", () => {
+  it("should calculate for average car value and risk rating", () => {
     // Arrange
     const car_value = 6600;
-    const risk_rating = 4;
+    const risk_rating = 3;
     const expected = { premiumData: { monthly_premium: 22, yearly_premium: 264 } };
 
     // Act
@@ -15,11 +15,11 @@ describe("premium quote tests", () => {
     expect(actual).toEqual(expected);
   });
 
-  it("should return error message. risk_rating > 8. ", () => {
+  it("should calculate for low car value and risk rating", () => {
     // Arrange
-    const car_value = 6600;
-    const risk_rating = 8;
-    const expected = { error: "Missing parameters" };
+    const car_value = 3000;
+    const risk_rating = 2;
+    const expected = { premiumData: { monthly_premium: 18, yearly_premium: 216 } };
 
     // Act
     const actual = calculateQuote(car_value, risk_rating);
@@ -28,11 +28,77 @@ describe("premium quote tests", () => {
     expect(actual).toEqual(expected);
   });
 
-  it("should return error message. risk_rating < 1. ", () => {
+  it("should calculate premium for a high car value and low risk rating", () => {
+    // Arrange
+    const car_value = 50000;
+    const risk_rating = 1;
+    const expected = { premiumData: { monthly_premium: 30, yearly_premium: 360 } };
+
+    // Act
+    const actual = calculateQuote(car_value, risk_rating);
+
+    // Assert
+    expect(actual).toEqual(expected);
+  });
+
+  it("should calculate premium for a low car value and high risk rating", () => {
+    // Arrange
+    const car_value = 2000;
+    const risk_rating = 5;
+    const expected = { premiumData: { monthly_premium: 40, yearly_premium: 480 } };
+
+    // Act
+    const actual = calculateQuote(car_value, risk_rating);
+
+    // Assert
+    expect(actual).toEqual(expected);
+  });
+
+  it("should return error message for car value exceeding maximum allowed", () => {
+    // Arrange
+    const car_value = 1000000000;
+    const risk_rating = 4;
+    const expected = { error: "Car value is too high" };
+
+    // Act
+    const actual = calculateQuote(car_value, risk_rating);
+
+    // Assert
+    expect(actual).toEqual(expected);
+  });
+
+
+  it("should return error message for negative car value", () => {
+    // Arrange
+    const car_value = -5000;
+    const risk_rating = 2;
+    const expected = { error: "Invalid car value" };
+
+    // Act
+    const actual = calculateQuote(car_value, risk_rating);
+
+    // Assert
+    expect(actual).toEqual(expected);
+  });
+
+  it("should return error message for risk rating > 5", () => {
+    // Arrange
+    const car_value = 6600;
+    const risk_rating = 6;
+    const expected = { error: "Invalid risk rating" };
+
+    // Act
+    const actual = calculateQuote(car_value, risk_rating);
+
+    // Assert
+    expect(actual).toEqual(expected);
+  });
+
+  it("should return error message for risk rating < 1", () => {
     // Arrange
     const car_value = 6600;
     const risk_rating = 0;
-    const expected = { error: "Missing parameters" };
+    const expected = { error: "Invalid risk rating" };
 
     // Act
     const actual = calculateQuote(car_value, risk_rating);
@@ -40,4 +106,57 @@ describe("premium quote tests", () => {
     // Assert
     expect(actual).toEqual(expected);
   });
+
+  it("should return error message for invalid risk rating type", () => {
+    // Arrange
+    const car_value = 7500;
+    const risk_rating: any = "high";
+    const expected = { error: "Invalid risk rating" };
+
+    // Act
+    const actual = calculateQuote(car_value, risk_rating);
+
+    // Assert
+    expect(actual).toEqual(expected);
+  });
+
+  it("should return error message for car value not provided", () => {
+    // Arrange
+    const car_value: any = "";
+    const risk_rating = 3;
+    const expected = { error: "Car value is required" };
+
+    // Act
+    const actual = calculateQuote(car_value, risk_rating);
+
+    // Assert
+    expect(actual).toEqual(expected);
+  });
+
+  it("should return error message for risk rating not provided", () => {
+    // Arrange
+    const car_value = 6000;
+    const risk_rating: any = "";
+    const expected = { error: "Risk rating is required" };
+
+    // Act
+    const actual = calculateQuote(car_value, risk_rating);
+
+    // Assert
+    expect(actual).toEqual(expected);
+  });
+
+  it("should return error message for both car value and risk rating not provided", () => {
+    // Arrange
+    const car_value: any = "";
+    const risk_rating: any = "";
+    const expected = { error: "Car value and risk rating are required" };
+
+    // Act
+    const actual = calculateQuote(car_value, risk_rating);
+
+    // Assert
+    expect(actual).toEqual(expected);
+  });
+
 });
